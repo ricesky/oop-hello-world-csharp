c69f024132861997e2594d07b296766d87b29053
rizky <rja@its.ac.id>
2025-03-03 12:05:33 +0700

    feat: add hello printer feature

--- src/HelloWorld/HelloWorld/Program.cs
+++ src/HelloWorld/HelloWorld/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace HelloWorld
+﻿using HelloLib;
+
+namespace HelloWorld
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Console.Write("Enter your name: ");
+            string? name = Console.ReadLine();
+
+            HelloPrinter printer = new HelloPrinter();
+
+            if (name == null)
+            {
+                Console.WriteLine("Please write something bro...");
+            }
+
+            printer.SayHello(name);
+
         }
     }
 }
